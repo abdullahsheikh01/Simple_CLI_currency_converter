b1445990b772d0581af55df87260e252178bd11c
abdullahsheikh01 <162831270+abdullahsheikh01@users.noreply.github.com>
2024-04-08 23:28:27 +0500

    First Commit
    This is First commit of this file.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Simple_CLI_currency_converter
 This is Simple currency converter in which you can do conversion of  multiple currencies .
+
+Its npx command is : npx currency_converter-6600.
+Its npm installation command is : npm i currency_converter-6600.cld
---
+++ index.ts
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+let currencyRates : any  = {
+   USD : 1 ,
+   PKR :278.40,
+   INR : 83.28,
+   EURO : 0.92,
+   SAR: 3.75,
+   CAD : 1.36, 
+}
+
+let userAnswers : any = await inquirer.prompt(
+    [
+        {
+            name : "fromCurrency",
+            type : "list",
+            choices : ["USD","PKR","INR","EURO","SAR","CAD"],
+            message :"Which currency you have to convert?"
+        },
+        {
+            name : "toCurrency",
+            type : "list",
+            choices : ["USD","PKR","INR","EURO","SAR","CAD"],
+            message :" In which currency you will like to convert?"
+        },
+        {
+            name : "amount",
+            type : "number",
+            message : "Enter the amount which  you have to convert : "
+        }
+    ]
+);
+let fromCurrencyy = currencyRates[userAnswers.fromCurrency];
+let  toCurrency = currencyRates[userAnswers.toCurrency];
+let baseCurrency = currencyRates.USD;
+let amount = userAnswers.amount;
+let baseAmount = amount/fromCurrencyy;
+let finalAmount = baseAmount * toCurrency ;
+console.log(`Your Converted Amount is ${finalAmount.toFixed(2)} ${userAnswers.toCurrency}`);
